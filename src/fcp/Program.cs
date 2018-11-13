656c5d1b4019e0fba48948b43b490c2e6cbd9917
ucavalcante <ziel7@hotmail.com>
2018-11-13 15:42:53 -0200

    Basic Command Driver implemented

--- src/fcp/Program.cs
+++ src/fcp/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace fcp
 {
     class Program
     {
+        private static int port = 1983;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandDriver(args);
         }
+
+        private static void CommandDriver(string[] args)
+        {
+            List<FileInfo> ArquivosList = new List<FileInfo>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arq = new FileInfo(args[i]);
+                if (arq.Exists)
+                {
+                    ArquivosList.Add(arq);
+                }
+
+                switch (args[i])
+                {
+                    case "-p":
+                        port = Convert.ToInt32(args[i + 1]);
+                        i++;
+                        break;
+                    default:
+                        Console.WriteLine($"-h|--help para ajuda");
+                        break;
+                }
+            }
+            if (ArquivosList.Count > 0)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+
     }
 }
